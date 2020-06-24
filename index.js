6e866682da2a1f45e6bb644486a4434f7f820289
sunteago <santi.vallejo@gmail.com>
2020-06-24 15:19:51 -0300

    Added cors

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors');
+
 const users = require('./routes/users');
 const auth = require('./routes/auth');
 const links = require('./routes/links');
@@ -7,9 +9,14 @@ const files = require('./routes/files');
 
 const app = express();
 
+const port = process.env.PORT || 4000;
+
 connectDB();
 
-const port = process.env.PORT || 4000;
+const corsOpts = {
+    origin: process.env.FRONTEND_URL
+}
+app.use(cors(corsOpts));
 
 app.use(express.json());
 
@@ -18,5 +25,8 @@ app.use('/api/auth', auth);
 app.use('/api/links', links);
 app.use('/api/files', files);
 
-app.listen(port, '0.0.0.0');
-console.log(`Server working in port ${port}`);
+
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server working in port ${port}`)
+});
